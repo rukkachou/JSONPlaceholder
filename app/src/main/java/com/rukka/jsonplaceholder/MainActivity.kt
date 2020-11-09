ee75d557e03678a4977b1afa0c7937c077dc3fab
rukkachou <rukka.chou@gmail.com>
2020-11-09 17:29:03 +0800

    Setup ActionBar with NavController. Add ScrollView in detail_fragment.xml.

--- app/src/main/java/com/rukka/jsonplaceholder/MainActivity.kt
+++ app/src/main/java/com/rukka/jsonplaceholder/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.rukka.jsonplaceholder
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.Navigation
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
+import com.rukka.jsonplaceholder.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        val navController = findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return this.findNavController(R.id.nav_host_fragment).navigateUp()
     }
 }
